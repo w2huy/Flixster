02d6ab4f17e0ef6ad1e090c6dd0ed9447eaff44f
w2huy <huynhwi2@msu.edu>
2019-10-23 10:29:53 -0400

    Segue: MovieGridCell -> MovieDetailsController

--- Flixster/MovieGridViewController.swift
+++ Flixster/MovieGridViewController.swift
@@ -71,14 +71,26 @@ class MovieGridViewController: UIViewController, UICollectionViewDataSource, UIC
         return cell
     }
     
-    /*
+    
      // MARK: - Navigation
      
      // In a storyboard-based application, you will often want to do a little preparation before navigation
      override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
      // Get the new view controller using segue.destination.
      // Pass the selected object to the new view controller.
+        
+        // Find selected movie
+        let cell = sender as! UICollectionViewCell
+        let indexPath = collectionView.indexPath(for: cell)!
+        let movie = movies[indexPath.row]
+        
+        // Pass the selected movie to the detials view controller
+        let detailVC = segue.destination as! MovieDetailsViewController
+        detailVC.movie = movie
+        
+        // Remove still hightlight of the cell after tap
+        collectionView.deselectItem(at: indexPath, animated: true)
      }
-     */
+     
     
 }
